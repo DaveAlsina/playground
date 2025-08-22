7d3828b56309ab9e341016774ed9c3e9219819a6
DaveAlsina <david.alsina@loka.com>
2025-08-22 04:29:40 -0500

    Update: some evil, unexpected code changes, you will have to live with for later

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # main.py
 
 # This is a line from the original branch
+
+#Now, I'm adding some code from the original branch that might be cumbersome to work with 
+#For the poor developer that now wants to rebase this thing
+
+#Evil indeed, but one needs to train for it
