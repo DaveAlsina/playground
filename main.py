fc67eb1ba41549116d41fc03b77e9dba4bbef2f9
DaveAlsina <david.alsina@loka.com>
2025-08-22 04:27:46 -0500

    Update: Implement the module A feature

--- main.py
+++ main.py
@@ -4,5 +4,12 @@
 
 #Now, I'm adding some code from the original branch that might be cumbersome to work with 
 #For the poor developer that now wants to rebase this thing
-
 #Evil indeed, but one needs to train for it
+# =======
+#Now I'm importing something new on this new feature
+#Now I'm importing something new on this new feature
+#Yeah, twice
+# =======
+
+# I'm leaving both changes, as them can coexist
+# Happy merge conflict resolution :)
